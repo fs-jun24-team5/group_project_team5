9080e0c8b32098dd2fd6ba60f3c420a3973c9a50
Vadym Babisov <vadim.babisov@gmail.com>
2024-09-12 17:34:18 +0300

    one more commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/https://github.com/fs-jun24-team5/group_project_team5/',  
+  build: {
+    outDir: 'dist',  
+  },
 })
