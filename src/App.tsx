4ea1eaf414e210d1ec8a8c0e1b5f1698a916676c
Yehor Volkov <volkov199009e@gmail.com>
2024-09-13 17:51:42 +0300

    add card layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 /* eslint-disable react/react-in-jsx-scope */
+import { Card } from './components/Card/Card';
 import './styles.scss'
 
 export const App = () => {
   return (
-    <>
-      <h1 className="title">Hello World!!!</h1>
-      <h1>Test 0001</h1>
-      <h1>Test 0002</h1>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png" alt="React Icon" />
-    </>
+    <Card/>
   );
 };
