9cc8bd0fc4285eff01f10e073f3d47d67aab25cc
YaroslavKolesnyk <kolesnik.y2004@gmail.com>
2024-09-13 17:43:05 +0300

    Header adaptive layout (with-out burger

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable react/react-in-jsx-scope */
 import './styles.scss'
+import { Header } from './сomponents/Header/Header';
+
 
 export const App = () => {
   return (
     <>
-      <h1 className="title">Hello World!!!</h1>
-      <h1>Test 0001</h1>
-      <h1>Test 0002</h1>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png" alt="React Icon" />
+      <Header />
+      <h1>Welcome to Nice Gadgets store!</h1>
     </>
   );
 };
