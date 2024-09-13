1b5c95e1621366b3c94dfd586adb316546c77ab0
Yehor Volkov <volkov199009e@gmail.com>
2024-09-13 11:30:21 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ export const App = () => {
     <>
       <h1>Hello World!!!</h1>
       <h1>Test 0001</h1>
+      <h1>Test 0002</h1>
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png" alt="React Icon" />
     </>
   );
