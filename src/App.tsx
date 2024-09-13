f9f7c18fe9a854f9ae29e0d17f08f6991d314b16
AndGrachov <grachovand@gmail.com>
2024-09-13 17:21:11 +0300

    add footer layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
+import { Footer } from './components/Footer/Footer';
 import './styles.scss'
+
 
 export const App = () => {
   return (
@@ -8,6 +10,7 @@ export const App = () => {
       <h1>Test 0001</h1>
       <h1>Test 0002</h1>
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png" alt="React Icon" />
+      <Footer />
     </>
   );
 };
