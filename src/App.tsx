12b2258ce163353994f5ecb24b30a0d6097eb136
YaroslavKolesnyk <kolesnik.y2004@gmail.com>
2024-09-13 11:55:39 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ export const App = () => {
       <h1>Hello World!!!</h1>
       <h1>Test 0001</h1>
       <h1>Test 0002</h1>
+      <h1>Test 0004fsasf</h1>
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png" alt="React Icon" />
     </>
   );
