5aab8a86b83f83807296a78f88d27ebb42b571c6
Vadym Babisov <vadim.babisov@gmail.com>
2024-09-13 10:38:38 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/react-in-jsx-scope */
 export const App = () => {
   return (
-    <h1>Hello World</h1>
-  )
+    <>
+      <h1>Hello World!!!</h1>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png" alt="React Icon" />
+    </>
+  );
 };
